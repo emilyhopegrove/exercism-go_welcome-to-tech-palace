22949e969c1dfaba0e79656dcc66ab322b1b4ecb
Emily Grove <eh.grove@yahoo.com>
2023-05-24 14:31:17 -0500

    Working solutions

--- welcome_to_tech_palace.go
+++ welcome_to_tech_palace.go
@@ -1,16 +1,26 @@
 package techpalace
+import (
+	"fmt"
+	"strings"
+)
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return fmt.Sprintf("Welcome to the Tech Palace, " + strings.ToUpper(customer))
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	repeatedStars := strings.Repeat("*", numStarsPerLine)
+	fancyWelcome := fmt.Sprintf("%s\n%s\n%s", repeatedStars, welcomeMsg, repeatedStars)
+	return fancyWelcome 
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	newMsg := strings.ReplaceAll(oldMsg, "*", "")
+	return strings.TrimSpace(newMsg)
 }
+
+
+
